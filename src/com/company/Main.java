89914a1e83d282ede96d88632dd43d4c2f64c36f
Doveryay <doveryay@gmail.com>
2017-01-09 19:05:12 +0300

    Добавил еще одну строку
    Добавил еще одну строку

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("May be");// write your code here
+        System.out.println("May be");
+        System.out.println("Probably");// write your code here
     }
 }
