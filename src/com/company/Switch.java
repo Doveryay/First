6aeb4ac690dd2c31078940078d1126950069e9dc
Doveryay <doveryay@gmail.com>
2017-01-10 18:47:03 +0300

    Тестовые изменения

--- src/com/company/Switch.java
+++ src/com/company/Switch.java
@@ -6,7 +6,7 @@ package com.company;
 public class Switch {
     public static void main(String[] args) {
 
-        System.out.println("no no no");
+        System.out.println("noo no no");
         System.out.println("Ну нет");
         System.out.println("Ну может быть");
     }
