df2431a118efd5691d5da135f9f0a56ba038f3ef
Doveryay <doveryay@gmail.com>
2017-01-10 17:43:12 +0300

    Тестовые изменения

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("May be");
-        System.out.println("Probably");// write your code here
+        System.out.println("May be me");
+        System.out.println("Probably you");// write your code here
     }
 }
